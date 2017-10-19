7ffa9ea3aef837352afb79195a645cfcaeac9878
小狄V5 <15038098307@163.com>
2017-10-19 16:31:29 +0800

    创建BaseActivity统一设置全屏显示

--- app/src/main/java/com/tongtong/pvzhmpro/MainActivity.java
+++ app/src/main/java/com/tongtong/pvzhmpro/MainActivity.java
@@ -1,9 +1,10 @@
 package com.tongtong.pvzhmpro;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tongtong.pvzhmpro.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/tongtong/pvzhmpro/base/BaseActivity.java
@@ -0,0 +1,23 @@
+package com.tongtong.pvzhmpro.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * 公共基类，实现子类共有方法
+ * Created by allen on 2017/10/19.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //设置无标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //设置全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
