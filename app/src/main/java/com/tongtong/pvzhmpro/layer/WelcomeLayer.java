17dfc552e0f4a9e53e93f6d978fe4e193d1b3d10
小狄V5 <15038098307@163.com>
2017-10-19 17:06:01 +0800

    启动页添加Logo和背景

--- app/src/main/java/com/tongtong/pvzhmpro/layer/WelcomeLayer.java
+++ app/src/main/java/com/tongtong/pvzhmpro/layer/WelcomeLayer.java
@@ -1,5 +1,9 @@
 package com.tongtong.pvzhmpro.layer;
 
+import org.cocos2d.actions.instant.CCCallFunc;
+import org.cocos2d.actions.instant.CCHide;
+import org.cocos2d.actions.interval.CCDelayTime;
+import org.cocos2d.actions.interval.CCSequence;
 import org.cocos2d.layers.CCLayer;
 import org.cocos2d.nodes.CCDirector;
 import org.cocos2d.nodes.CCSprite;
@@ -21,6 +25,10 @@ public class WelcomeLayer extends CCLayer {
 
     /*初始化*/
     private void init() {
+        logo();
+    }
+
+    private void logo() {
         //创建Logo精灵
         CCSprite logo = CCSprite.sprite("image/popcap_logo.png");
         //获取屏幕尺寸
@@ -29,5 +37,25 @@ public class WelcomeLayer extends CCLayer {
         logo.setPosition(winSize.width / 2, winSize.height / 2);
         //添加到图层
         this.addChild(logo);
+        /*让Logo执行动作（展示一秒隐藏）*/
+        //隐藏
+        CCHide ccHide = CCHide.action();
+        //停留一秒
+        CCDelayTime delayTime = CCDelayTime.action(1);
+        //动作串行
+        CCSequence ccSequence = CCSequence.actions(delayTime, delayTime,
+                ccHide, delayTime, CCCallFunc.action(this, "loadWelcome"));
+        //运行动作
+        logo.runAction(ccSequence);
+    }
+
+    //当动作执行完了之后调用
+    public void loadWelcome() {
+        //创建背景精灵
+        CCSprite bg = CCSprite.sprite("image/welcome.jpg");
+        //设置背景精灵锚点为左下角
+        bg.setAnchorPoint(0, 0);
+        //添加到图层
+        this.addChild(bg);
     }
 }
